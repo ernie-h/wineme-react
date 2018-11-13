6e804126c123e4d8b2625f4d559560a1616b6d37
sophia <ho.so@husky.neu.edu>
2018-11-13 14:05:33 -0500

    added ant design library

--- src/containers/LoginLayout.jsx
+++ src/containers/LoginLayout.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Button, Form, Input } from 'antd';
+import "antd/dist/antd.css";
+
+const FormItem = Form.Item;
 
 export class LoginLayout extends Component {
   render() {
     return (
       <div className='center'>
           <h1 className='title'> Wine me </h1>
-        
+          <Button type='primary'>Test</Button> 
+
+          <Form className='login-form'>
+          <FormItem>
+            <Input placeholder='username' />
+          </FormItem>
+          <FormItem>
+            <Input type='password' placeholder='password' />
+          </FormItem>
+          </Form>
       </div>
     );
   }
