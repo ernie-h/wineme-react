a5b0c0d3f2f562f7cc7c1d8d6897905541d545f8
ernie-hao <pdphiro@gmail.com>
2018-11-13 13:14:37 -0500

    update

--- src/containers/LoginLayout.jsx
+++ src/containers/LoginLayout.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class LoginLayout extends Component {
   render() {
     return (
-      <div className='center'>
-          <h1 className='title'> Wine me </h1>
-        
+      <div className='row'>
+        <div className='col'>
+          &nbsp;
+        </div>
+        <div className='col-1-md mt-5'>
+          <h1 className='title'>
+            Wine me
+          </h1>
+        </div>
+        <div className='col'>
+          &nbsp;
+        </div>
       </div>
     );
   }
